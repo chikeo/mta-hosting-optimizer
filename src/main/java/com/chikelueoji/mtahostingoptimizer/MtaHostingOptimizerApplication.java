125ce76ce32f29b0c528e2d512ef9082295b4dd8
Chikelue Oji <chike_oji@yahoo.com>
2018-07-03 00:08:03 +0100

    Created mta-hosting-optimizer application

---
+++ src/main/java/com/chikelueoji/mtahostingoptimizer/MtaHostingOptimizerApplication.java
@@ -0,0 +1,12 @@
+package com.chikelueoji.mtahostingoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtaHostingOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtaHostingOptimizerApplication.class, args);
+	}
+}
